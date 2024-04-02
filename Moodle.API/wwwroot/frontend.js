1e4c68425e4dc5041d0cf4d14c46ac2e355f82f0
ViAron04 <viragharon04@gmail.com>
2024-04-02 23:42:12 +0200

    egyesítés 1

--- Moodle.API/wwwroot/frontend.js
+++ Moodle.API/wwwroot/frontend.js
@@ -1,19 +1,21 @@
 
 function bejelentkezes(){
-const url = "http://localhost:7090/api/Test";
+
+    
+    const url = "http://localhost:7090/api/Test";
 
 fetch(url)
     .then(response => {
-        // Check if the request was successful (status code 200)
+        
         if (response.ok) {
-            // Parse the response as JSON (or text, if needed)
+        
             return response.json();
         } else {
             throw new Error(`Error: Status code ${response.status}`);
         }
     })
     .then(data => {
-        // Handle the retrieved data
+       
         console.log("Response data:", data);
     })
     .catch(error => {
