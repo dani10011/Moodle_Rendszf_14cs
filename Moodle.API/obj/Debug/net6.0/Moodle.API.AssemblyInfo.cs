0982ac94fe41e70034c66c29ca805102c86b7774
Dániel Ökrös <okros.dani28@gmail.com>
2024-04-08 20:49:56 +0200

    CourseController létrehozása, mainPage.html script includolás kijavítása

--- Moodle.API/obj/Debug/net6.0/Moodle.API.AssemblyInfo.cs
+++ Moodle.API/obj/Debug/net6.0/Moodle.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Moodle.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08235fb9ae281f4537307b6713dc4026b7d4ff10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Moodle.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Moodle.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
