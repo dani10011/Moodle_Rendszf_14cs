9d6cebc9ad84ea8139b7e0d95e4a1cc821ad7a0c
KLB <kovacs.bendeguz188@gmail.com>
2024-04-10 19:38:12 +0200

    Course illetve User class hozzáadva, valamint AuthenticationController is hozzáadva

---
+++ Moodle.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Moodle.API.Controllers
+{
+    public class AuthenticationController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            
+        }
+    }
+}
