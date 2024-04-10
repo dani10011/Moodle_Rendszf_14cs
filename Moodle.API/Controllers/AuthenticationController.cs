82e931e5387873ed99b9a96955daf84e35f73ce9
KLB <kovacs.bendeguz188@gmail.com>
2024-04-10 22:55:00 +0200

    Autentikáció próba

--- Moodle.API/Controllers/AuthenticationController.cs
+++ Moodle.API/Controllers/AuthenticationController.cs
@@ -1,22 +1,30 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using static Moodle.Core.Authentication;
+using System.Text.Json;
 
 
 namespace Moodle.API.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class AuthenticationController : ControllerBase
     {
         
         [HttpPost("login")]
-        public IActionResult PostData([FromBody] Moodle.Core.Authentication data)
+        public IActionResult PostData([FromBody] Core.Authentication data)
         {
+
             // Now you can access data.Field1 and data.Field2
-            
+
             // Do something with the data...
 
-            return Ok(); // Return a 200 OK response
+            //teszt
+            string jsonString = "sikeres json atadas ujra!!!!!";
+            string json = JsonSerializer.Serialize(jsonString);
+            return this.Content(json, "application/json");
+            //
+
+            //return Ok(); // Return a 200 OK response
         }
     }
 }
