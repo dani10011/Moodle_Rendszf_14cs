c093068fbc8a8d71ba09b791c21d29b31807f6ec
ViAron04 <viragharon04@gmail.com>
2024-04-11 05:18:15 +0200

    Végleges jsonnel funkciók implementálása, saját kurzusok kilistázása

--- Moodle.API/obj/Debug/net6.0/Moodle.API.AssemblyInfo.cs
+++ Moodle.API/obj/Debug/net6.0/Moodle.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Moodle.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75a69dd20ea1f15de18b53fbedbc7cbff08cabed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a786e9b89bca7cf873f4da7b36aaeffd3817255f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Moodle.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Moodle.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Moodle.API/wwwroot/test.js
+++ Moodle.API/wwwroot/test.js
@@ -19,7 +19,7 @@ function bejelentkezes(){
 }
 
 function atvitel(){
-    const url = "https://localhost:7090/api/Test/probajson";
+    const url = "https://localhost:7090/api/Course/allcourses";
     fetch(url)
   .then(response => {
     if (!response.ok) {
@@ -57,14 +57,52 @@ function atvitel(){
   });
 }
 
+function sajatlista(){
+  const url = "https://localhost:7090/api/Course/courseid";
+  fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data); // Log the content of the JSON file to the console
+    const dataDisplay = document.getElementById("dataDisplay");
+    dataDisplay.innerHTML = '';
+    data.forEach(item => {
+         
+      const nameElement = document.createElement("p");
+      nameElement.textContent = "name: " + item.name;
+    
+      const ageElement = document.createElement("p");
+      ageElement.textContent = "code: " + item.code;
+    
+      const departmentElement = document.createElement("p");
+      departmentElement.textContent = "department: " + item.department;
 
+      const creditElement = document.createElement("p");
+      creditElement.textContent = "credit: " + item.credit;
+    
+      dataDisplay.appendChild(nameElement);
+      dataDisplay.appendChild(ageElement);
+      dataDisplay.appendChild(departmentElement);
+      dataDisplay.appendChild(creditElement);
 
+    });
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
+
+}
+/*
 function tanszszures(){
     var tanszekmezo = document.getElementById('tansz');
     var tanszekszur = tanszekmezo.value;
     console.log(tanszekszur);
 
-    const url = "https://localhost:7090/api/Test/probajson";
+    const url = "https://localhost:7090/api/Course/allcourses";
     fetch(url)
   .then(response => {
     if (!response.ok) {
@@ -102,7 +140,7 @@ function tanszszures(){
     console.error('There was a problem with the fetch operation:', error);
   });
 }
-
+*/
 
 
 
