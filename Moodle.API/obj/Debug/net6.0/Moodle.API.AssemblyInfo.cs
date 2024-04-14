536c658956520f33af03e921a4566c983eeb96d1
KLB <kovacs.bendeguz188@gmail.com>
2024-04-14 23:45:35 +0200

    MoodleDbContext osztály létrehozva, valamint új Nuget package-ek telepítve (EntityFrameworkCore)

--- Moodle.API/obj/Debug/net6.0/Moodle.API.AssemblyInfo.cs
+++ Moodle.API/obj/Debug/net6.0/Moodle.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Moodle.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31e8cbaae4e6b61f5267efeabf3f1ff361c59f26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2938b1b93826f7263b01391291e70b1c45f14eb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Moodle.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Moodle.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Moodle.Data/obj/Debug/net6.0/Moodle.Data.AssemblyInfo.cs
+++ Moodle.Data/obj/Debug/net6.0/Moodle.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Moodle.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2938b1b93826f7263b01391291e70b1c45f14eb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Moodle.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Moodle.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
