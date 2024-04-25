97868680d53731c9eb6c305c4b9c12c635e4c5cf
ViAron04 <viragharon04@gmail.com>
2024-04-25 04:43:48 +0200

    Event felvétel teszt

--- Moodle.API/wwwroot/frontend.js
+++ Moodle.API/wwwroot/frontend.js
@@ -353,12 +353,103 @@ form.className="osszForm";
 
 
 
-
-function esemenyLetrehozas() {
-
+//új esemény létrehozása (csak tanár)
+async function esemenyLetrehozas() {
+  const retrievedData = sessionStorage.getItem('currentUserId');
+  var url = "https://localhost:7090/api/Course/courseid?id=" + retrievedData;
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    const dataDisplay = document.getElementById("dataDisplay");
+    dataDisplay.innerHTML = '';
+    const ul = document.createElement('ul');
+    data.forEach(item => {
+      const li = document.createElement('li'); // elemenként egy li
+      li.textContent = `${item.Name} (${item.Code}, ${item.Department}), kredit: ${item.Credit}`;
+      li.addEventListener('click', () => {
+        console.log(item.Id);
+        esemenyForm(item.Id);
+      });
+      ul.appendChild(li);
+    });
+    dataDisplay.appendChild(ul);
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
 }
 
 
+//megjeleníti a form-ot új esemény létrehozásáshoz
+async function esemenyForm(course_id){
+  //course_id!!!!!!!!!!!
+
+  const dataDisplay = document.getElementById("dataDisplay");
+  dataDisplay.innerHTML = '';
+
+  // Elkészíti a formot
+  const form = document.createElement("form");
+  form.className="osszForm";
+
+  // Input mezőket csinál
+  //name
+  const inputName = document.createElement("input");
+  inputName.type = "text";
+  inputName.id = "nameId";
+  inputName.className = "formClass";
+  inputName.placeholder = "Esemény neve";
+
+  //description
+  const inputDesc = document.createElement("input");
+  inputDesc.type = "text";
+  inputDesc.id = "descId";
+  inputDesc.className = "formClass";
+  inputDesc.placeholder = "Esemény leírása";
+
+  // Létrehozás gomb
+  const submitButton = document.createElement("button");
+  submitButton.type = "submit";
+  submitButton.textContent = "Kurzus létrehozása";
+  submitButton.addEventListener('click', async () => {
+    
+    var name = document.getElementById(nameId);
+    var description = document.getElementById(descId);
+
+    const AddEvent = {
+      course_id: course_id,
+      name: name,
+      description: description
+    };
+  
+    const response = await fetch('https://localhost:7090/api/Authentication/AddEvent', { //elküldi az adatokat, await megállítja a funkciót amíg válaszra vár
+      method: 'POST',
+      body: JSON.stringify(AddEvent),
+      headers: { 'Content-Type': 'application/json' }
+    });
+  
+    if (!response.ok) { //ellenőrzi a login válaszát
+      const message = await response.text();
+      alert(message);
+    } 
+    else {
+      const data = await response.json(); // Parse the JSON response
+      const message = data.message;
+
+      alert(message);    
+    }
+  })
+
+  // Hozzáadás a form-hoz
+  form.appendChild(inputName);
+  form.appendChild(inputDesc);
+  form.appendChild(submitButton);
+  dataDisplay.appendChild(form);
+}
 
 
 
